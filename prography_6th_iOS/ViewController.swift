9561ec2b4134d724d9d933a482eb5e3eb0e82ef1
HeoGwangHo <gjrhkdgh3@naver.com>
2020-03-02 17:37:03 +0900

    init commit

---
+++ prography_6th_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  prography_6th_iOS
+//
+//  Created by Gwang_Ho on 02/03/2020.
+//  Copyright © 2020 Gwang-Ho Heo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
